ab36b26059315d4da0654fc52d4c7af3521028f5
AAspan <93344588+AAspan@users.noreply.github.com>
2022-07-26 12:28:23 -0400

    upc

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@ namespace app{
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Int32 num=30;
+                 Console.Write(num);  
+                 Console.WriteLine();
+                 
+            double numm=30.33;
+                Console.Write(numm); 
+                Console.WriteLine();
+
+            Boolean status=true;
+            Console.Out.NewLine = "\r\n\r\n";
+                Console.Write(status);  
+                Console.WriteLine();
+                    
+                    
+            String message="Hello";
+                 Console.Write(message);
         }
     }
 }
